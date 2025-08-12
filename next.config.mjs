8a5f3ff71de508799729ecede6695b4bbb1f9e4c
S SIDHARTH <92774572+callmesidhu@users.noreply.github.com>
2025-08-13 00:28:03 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    optimizeCss: false, // disables Lightning CSS, uses PostCSS instead
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
